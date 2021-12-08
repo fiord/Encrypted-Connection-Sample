ea62d234f38d15bc22d12265687c17873363215b
fiord <fiord@users.noreply.github.com>
2021-12-09 01:43:32 +0900

    feature: aes gcm server

--- cmd/main.go
+++ cmd/main.go
@@ -1,35 +1,94 @@
 package main
 
 import (
-  // "crypto/aes"
-  // "crypto/cipher"
-  // "crypto/rand"
+  "crypto/aes"
+  "crypto/cipher"
+  "crypto/rand"
   "encoding/base64"
   "fmt"
+  "io"
   "os"
   "net/http"
   "github.com/gin-gonic/gin"
 )
 
+
+// cipher
+var KEY = []byte("FLAG{here_15_5ecret_ky4_12345ab}")
+
+func encrypt(s string) (string, error) {
+  c, err := aes.NewCipher(KEY)
+  if err != nil {
+    return "", err
+  }
+
+  gcm, err := cipher.NewGCM(c)
+  if err != nil {
+    return "", err
+  }
+
+  nonce := make([]byte, gcm.NonceSize())
+  if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+    return "", err
+  }
+
+  out := gcm.Seal(nonce, nonce, []byte(s), nil)
+  out_enc := base64.StdEncoding.EncodeToString(out)
+  return out_enc, err
+}
+
+func decrypt(param_name string) ([]byte, error) {
+  enc_name_byte, err := base64.StdEncoding.DecodeString(param_name)
+  if err != nil {
+    return []byte{}, err
+  }
+
+  c, err := aes.NewCipher(KEY)
+  if err != nil {
+    return []byte{}, err
+  }
+
+  gcm, err := cipher.NewGCM(c)
+  if err != nil {
+    return []byte{}, err
+  }
+
+  nonceSize := gcm.NonceSize()
+  if len(enc_name_byte) < nonceSize {
+    return []byte{}, fmt.Errorf("too short param")
+  }
+
+  nonce, cipherText := enc_name_byte[:nonceSize], enc_name_byte[nonceSize:]
+  plain, err := gcm.Open(nil, nonce, cipherText, nil)
+  if err != nil {
+    return []byte{}, err
+  }
+
+  return plain, err
+}
+
 func main() {
-  // cipher
-  // key := []byte("FLAG{here_15_5ecret_ky4_12345ab}")
 
   // web server
   r := gin.Default()
 
   r.POST("/greet", func(c *gin.Context) {
     param_name := c.PostForm("name")
-    // decrypt
-    enc_name_byte, err := base64.StdEncoding.DecodeString(param_name)
+    decrypted, err := decrypt(param_name)
     if err != nil {
-      c.String(http.StatusBadRequest, "failed in base64.decode")
+      c.String(http.StatusBadRequest, err.Error())
       return
     }
 
-    res := []byte(fmt.Sprintf("Hello, %s!", enc_name_byte))
-    res_encoded := base64.StdEncoding.EncodeToString(res)
-    c.String(http.StatusOK, res_encoded)
+    res := []byte(fmt.Sprintf("Hello, %s!", decrypted))
+
+    enc, err := encrypt(string(res))
+    if err != nil {
+      c.String(http.StatusBadRequest, err.Error())
+      return
+    }
+
+    c.String(http.StatusOK, enc)
   })
 
   r.GET("/", func(c *gin.Context) {
